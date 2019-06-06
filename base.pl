9e927a7b5a34c1cf2edab0af1bde54221c12e152
Caio <caio23guimaraes@gmail.com>
2019-06-05 21:37:38 -0300

    regra de participação

--- base.pl
+++ base.pl
@@ -4,7 +4,7 @@ associacao(brasil, cbf).
 confederacao(brasil, conmebol).
 apelido(brasil, "Seleção Canarinho").
 
-%selecao(PAIS)
+%selecao(país)
 selecao(brasil).
 selecao(alemanha).
 selecao(italia).
@@ -16,10 +16,11 @@ selecao(argentina).
 selecao(hungria).
 selecao(inglaterra).
 selecao(portugal).
+selecao(suecia).
 
 %Fatos
 
-%participou(SELECAO, COMPETICAO, ANO)
+%participou(seleção, competição, ano)
 participou(brasil, copadomundo, 1930).
 participou(brasil, copadomundo, 1934).
 participou(brasil, copadomundo, 1938).
@@ -41,9 +42,43 @@ participou(brasil, copadomundo, 2006).
 participou(brasil, copadomundo, 2010).
 participou(brasil, copadomundo, 2014).
 participou(brasil, copadomundo, 2018).
-participou(alemanha, copadomundo, 2014).
 
-%campea(SELECAO, COMPETICAO, ANO)
+participou(espanha, copadomundo, 1934).
+participou(espanha, copadomundo, 1950).
+participou(suecia, copadomundo, 1938).
+participou(suecia, copadomundo, 1950).
+participou(hungria, copadomundo, 1954).
+participou(hungria, copadomundo, 1966).
+participou(inglaterra, copadomundo, 1958).
+participou(inglaterra, copadomundo, 1962).
+participou(portugal, copadomundo, 1966).
+participou(alemanha, copadomundo, 1966).
+participou(uruguai, copadomundo, 1970).
+participou(italia, copadomundo, 1970).
+participou(argentina, copadomundo, 1974).
+participou(holanda, copadomundo, 1974).
+participou(suecia, copadomundo, 1978).
+participou(espanha, copadomundo, 1978).
+participou(argentina, copadomundo, 1982).
+participou(franca, copadomundo, 1982).
+participou(alemanha, copadomundo, 1982).
+participou(espanha, copadomundo, 1986).
+participou(franca, copadomundo, 1986).
+participou(argentina, copadomundo, 1990).
+participou(suecia, copadomundo, 1994).
+participou(holanda, copadomundo, 1994).
+participou(italia, copadomundo, 1994).
+participou(holanda, copadomundo, 1998).
+participou(inglaterra, copadomundo, 2002).
+participou(alemanha, copadomundo, 2002).
+participou(franca, copadomundo, 2006).
+participou(inglaterra, copadomundo, 2006).
+
+
+%se uma selecão ganhou uma competição entao ela participou da mesma
+participou(SELECAO, COMPETICAO, ANO) :- ganhou(SELECAO, COMPETICAO, ANO).
+
+%venceu(seleção, competição, ano)
 venceu(uruguai, copadomundo, 1930).
 venceu(italia, copadomundo, 1934).
 venceu(italia, copadomundo, 1938).
