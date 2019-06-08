8accc2d3991554234ad4835280dbc451c8174dc2
Nicholas Campos Barcelos <nicholascb@id.uff.br>
2019-06-08 19:22:30 -0300

    adiciona algumas curiosidades

--- src/base.pl
+++ src/base.pl
@@ -19,6 +19,10 @@ selecao(inglaterra).
 selecao(portugal).
 selecao(suecia).
 selecao(iugoslavia).
+selecao(polonia).
+selecao(tchecoslovaquia).
+selecao(turquia).
+selecao(noruega).
 
 /* maiores_jogadores(JOGADOR, SELECAO) */
 maiores_jogadores(pele, brasil).
@@ -293,6 +297,48 @@ artilheiros(jairizinho, 44).
 artilheiros(rivelino, 43).
 artilheiros(bebeto, 39).
 artilheiros(rivaldo, 37).
+
+/*serie sem derrota(jogos,desde,adversario_ultima,ate,adversario_fim)*/
+sem_derrota(13,1954,hungria,1966,hungria).
+sem_derrota(11,1966,portugal,1974,holanda).
+sem_derrota(11,1974,polonia,2082,italia).
+sem_derrota(11,1998,franca,2006,franca).
+
+/*copas em que brasil enfrentou 2x o mesmo adversario(adversario, ano)*/
+duas_vezes(tchecoslovaquia, 1938).
+duas_vezes(tchecoslovaquia, 1962).
+duas_vezes(suecia, 1994).
+duas_vezes(turquia, 2002).
+
+/*finais com diferenca de gols 3 que o brasil participou(adversario,adversario_gols,brasil_gols,ano)*/
+final_dif_tres(suecia,2,5,1958).
+final_dif_tres(italia,1,4,1970).
+final_dif_tres(franca,3,0,1998).
+
+/*derrotas na fase de grupos da copa(derrotas,ano)*/
+derrota_grupo(3,1950).
+derrota_grupo(2,1966).
+derrota_grupo(1,1998).
+
+/*copas que o brasil for derrotado 2x(ano, prim_adversario, gols_prim, gols_br, sec_adversario, gols_sec, gols_br)*/
+copa_derrotado_dois(1966, hungria,3,1,portugal,3,1).
+copa_derrotado_dois(1974, holanda,2,0,polonia1,1,0).
+copa_derrotado_dois(1998, noruega,2,1,franca,3,0).
+copa_derrotado_dois(2014, alemanha,7,1,holanda,3,0).
+
+/*derrotas na estreia copa do mundo(ano,adversario,gols_ad,gols_br)*/
+derrrota_estreia(1930, iugoslavia,2,1).
+derrrota_estreia(1934, espanha,3,1).
+
+/*empates na estreia copa do mundo(ano,adversario,gols)*/
+derrrota_estreia(1974, iugoslavia,0).
+derrrota_estreia(1978, suecia,1).
+derrrota_estreia(2018, suica,1).
+
+/* copas em que o brasil jogou pelo 3o lugar (ano,adversario, gols_ad, gols_br)*/
+disputou_terceiro(1974, polonia, 1, 0).
+disputou_terceiro(1978, italia, 1, 2).
+
 
 
 
